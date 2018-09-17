666f291450a3814106e489546dc077b853ca77c2
Oliver Barnwell <barnwelloliver@gmail.com>
2018-09-17 23:37:02 +0100

    fix: breakpoints for gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -13,6 +13,31 @@ class Gallery extends React.Component {
       autoplay: true,
       autoplaySpeed: 10000,
       arrows: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+          },
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2,
+          },
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
     };
     return (
       <div className={this.props.className}>
