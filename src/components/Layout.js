2a5f1468e41b29b187c250805a6cc5b8103ea681
Oliver Barnwell <barnwelloliver@gmail.com>
2018-09-25 17:07:20 +0100

    feat: add working favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,9 @@ import './all.scss';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="R. U. Hacking?" />
+    <Helmet title="R. U. Hacking?">
+      <link rel="shortcut icon" href="favicon.png" />
+    </Helmet>
     {/* <Navbar /> */}
     <div>{children}</div>
     <Footer />
