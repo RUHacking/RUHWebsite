9c73f1fc05f47f999bfd40eb5bd8233e1d58affe
Oliver Barnwell <barnwelloliver@gmail.com>
2018-11-26 17:45:42 +0000

    feat: override default html.js

---
+++ src/html.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default class HTML extends React.Component {
+  render() {
+    return (
+      <html {...this.props.htmlAttributes}>
+        <head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+          {this.props.headComponents}
+        </head>
+        <body {...this.props.bodyAttributes}>
+          {this.props.preBodyComponents}
+          <div
+            key={'body'}
+            id="___gatsby"
+            dangerouslySetInnerHTML={{ __html: this.props.body }}
+          />
+          {this.props.postBodyComponents}
+          <a
+            id="mlh-trust-badge"
+            style={{
+              display: 'block',
+              maxWidth: '100px',
+              minWidth: '60px',
+              position: 'fixed',
+              right: '50px',
+              top: 0,
+              width: '10%',
+              zIndex: 10000,
+            }}
+            href="https://mlh.io/seasons/eu-2019/events?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2019-season&utm_content=black"
+            target="_blank"
+          >
+            <img
+              src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-black.svg"
+              alt="Major League Hacking 2019 Hackathon Season"
+              style={{ width: '100%' }}
+            />
+          </a>
+        </body>
+      </html>
+    );
+  }
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+};
