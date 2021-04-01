c54b007f1c2b42d97b0ae0cfc7bd9969f1483151
MilanRadojicic1 <78311149+MilanRadojicic1@users.noreply.github.com>
2021-04-01 17:33:33 +0100

    Small changes

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,12 +1,7 @@
 import React from 'react';
-import { Container } from '../../global_styles';
 
 const HomePage = () => {
-  return (
-    <>
-      <Container>HEY</Container>
-    </>
-  );
+  return <div>MILAN</div>;
 };
 
 export default HomePage;
