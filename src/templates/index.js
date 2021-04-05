02f1d02021f20ad844b19f2ea98d991c58723585
MilanRadojicic1 <78311149+MilanRadojicic1@users.noreply.github.com>
2021-04-05 16:57:07 +0100

    gatsby

--- src/templates/index.js
+++ src/templates/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Navbar, Footer } from '../components';
 import Home from './HomePage/Home';
 import 'react-awesome-button/dist/styles.css';
-import { graphql } from 'gatsby';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -21,65 +20,3 @@ function App() {
 }
 
 export default App;
-
-export const indexPageQuery = graphql`
-  query IndexPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
-      frontmatter {
-        title
-        hero {
-          title
-          description
-        }
-        loc {
-          title
-          description
-          marker
-          coords {
-            lat
-            lng
-          }
-        }
-        events {
-          title
-          desc
-          image
-          link
-          subtitle
-        }
-        featured_event {
-          title
-          desc
-          image
-          link
-          subtitle
-        }
-        featured_images {
-          image_list
-        }
-        sponsors {
-          title
-          description
-          sponsor_list {
-            image
-            link
-            tier
-            alt
-          }
-        }
-        about {
-          title
-          col_1 {
-            title
-            description
-          }
-          col_2 {
-            title
-            description
-          }
-        }
-      }
-    }
-  }
-`;
