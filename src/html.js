01de7f697f512071d5095f39f9b824a3c6e0ddd9
MilanRadojicic1 <78311149+MilanRadojicic1@users.noreply.github.com>
2021-04-27 12:07:26 +0100

    Revert "Removed MLH and changed year"
    This reverts commit 7e2d46b1863184a98d0e7fda869975f2d6c23dad.

--- src/html.js
+++ src/html.js
@@ -22,7 +22,27 @@ export default class HTML extends React.Component {
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
           {this.props.postBodyComponents}
-         
+          <a
+            id="mlh-trust-badge"
+            style={{
+              display: 'block',
+              maxWidth: '100px',
+              minWidth: '60px',
+              position: 'fixed',
+              right: '50px',
+              top: 0,
+              width: '10%',
+              zIndex: 10000,
+            }}
+            href="https://mlh.io/seasons/eu-2019/events?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2019-season&utm_content=black"
+            target="_blank"
+          >
+            <img
+              src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-black.svg"
+              alt="Major League Hacking 2019 Hackathon Season"
+              style={{ width: '100%' }}
+            />
+          </a>
         </body>
       </html>
     );
