28e43460d573e33db455e110be392f7d7b943d75
ej016289 <ej016289@student.reading.ac.uk>
2022-06-08 13:36:25 +0100

    Fixes sign-up link and updates MLH badge

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,174 +4,180 @@ import { IconContext } from 'react-icons/lib';
 import { Button } from '../../global_styles';
 import logo from '../../img/logo/newLogo.png';
 import {
-  ICON,
-  Nav,
-  NavbarContain,
-  NavBtnLink,
-  NavItem,
-  NavItemBtn,
-  NavLinks,
-  NavLogo,
-  NavMenu
+	ICON,
+	Nav,
+	NavbarContain,
+	NavItem,
+	NavItemBtn,
+	NavLinks,
+	NavLogo,
+	NavMenu
 } from './navbar.elements';
 
 const Navbar = () => {
-  const [click, setClick] = useState(false);
-  const [button, setButton] = useState(true);
-  const handleClick = () => setClick(!click);
+	const [click, setClick] = useState(false);
+	const [button, setButton] = useState(true);
+	const handleClick = () => setClick(!click);
 
-  const showButton = () => {
-    if (window.innetWidth <= 960) {
-      setButton(false);
-    } else {
-      setButton(true);
-    }
-  };
+	const showButton = () => {
+		if (window.innetWidth <= 960) {
+			setButton(false);
+		} else {
+			setButton(true);
+		}
+	};
 
-  useEffect(() => {
-    showButton();
-  }, []);
+	useEffect(() => {
+		showButton();
+	}, []);
 
-  window.addEventListener('resize', showButton);
+	window.addEventListener('resize', showButton);
 
-  return (
-    <>
-      <IconContext.Provider value={{ color: '#ea5837 ' }}>
-        <Nav>
-          <NavbarContain>
-            <NavLogo>
-              <img
-                src={logo}
-                alt="RUHacking Logo"
-                style={{ width: 'auto', height: 70, marginRight: 7 }}
-              />
-              <NavLinks
-                style={{
-                  color: '#ffff',
-                  fontfamily: ' clone-rounded-latin, sans-serif',
-                }}
-              >
-                R. U. Hacking?
-              </NavLinks>
-            </NavLogo>
+	return (
+		<>
+			<IconContext.Provider value={{ color: '#ea5837 ' }}>
+				<Nav>
+					<NavbarContain>
+						<NavLogo>
+							<img
+								src={logo}
+								alt="RUHacking Logo"
+								style={{
+									width: 'auto',
+									height: 70,
+									marginRight: 7,
+								}}
+							/>
+							<NavLinks
+								style={{
+									color: '#ffff',
+									fontfamily:
+										' clone-rounded-latin, sans-serif',
+								}}
+							>
+								R. U. Hacking?
+							</NavLinks>
+						</NavLogo>
 
-            <ICON onClick={handleClick}>
-              {click ? <FaTimes /> : <FaBars />}
-            </ICON>
-            <NavMenu onClick={handleClick} click={click}>
-              <NavItem>
-                <NavLinks
-                  onClick={handleClick}
-                  style={{
-                    color: '#ffff',
-                    fontfamily: ' clone-rounded-latin, sans-serif',
-                  }}
-                  to="Homepage"
-                >
-                  HOME
-                </NavLinks>
-              </NavItem>
+						<ICON onClick={handleClick}>
+							{click ? <FaTimes /> : <FaBars />}
+						</ICON>
+						<NavMenu onClick={handleClick} click={click}>
+							<NavItem>
+								<NavLinks
+									onClick={handleClick}
+									style={{
+										color: '#ffff',
+										fontfamily:
+											' clone-rounded-latin, sans-serif',
+									}}
+									to="Homepage"
+								>
+									HOME
+								</NavLinks>
+							</NavItem>
 
-              <NavItem>
-                <NavLinks
-                  onClick={handleClick}
-                  style={{
-                    color: '#ffff',
-                    fontfamily: ' clone-rounded-latin, sans-serif',
-                  }}
-                  to="faq"
-                >
-                  FAQ
-                </NavLinks>
-              </NavItem>
+							<NavItem>
+								<NavLinks
+									onClick={handleClick}
+									style={{
+										color: '#ffff',
+										fontfamily:
+											' clone-rounded-latin, sans-serif',
+									}}
+									to="faq"
+								>
+									FAQ
+								</NavLinks>
+							</NavItem>
 
-              <NavItem>
-                <NavLinks
-                  onClick={handleClick}
-                  style={{
-                    color: '#ffff',
-                    fontfamily: ' clone-rounded-latin, sans-serif',
-                  }}
-                  to="inf2"
-                >
-                  SPONSORS
-                </NavLinks>
-              </NavItem>
+							<NavItem>
+								<NavLinks
+									onClick={handleClick}
+									style={{
+										color: '#ffff',
+										fontfamily:
+											' clone-rounded-latin, sans-serif',
+									}}
+									to="inf2"
+								>
+									SPONSORS
+								</NavLinks>
+							</NavItem>
 
-              <NavItem>
-                <NavLinks
-                  onClick={handleClick}
-                  style={{
-                    color: '#ffff',
-                    fontfamily: ' clone-rounded-latin, sans-serif',
-                  }}
-                  to="inf1"
-                >
-                  ABOUT US
-                </NavLinks>
-              </NavItem>
+							<NavItem>
+								<NavLinks
+									onClick={handleClick}
+									style={{
+										color: '#ffff',
+										fontfamily:
+											' clone-rounded-latin, sans-serif',
+									}}
+									to="inf1"
+								>
+									ABOUT US
+								</NavLinks>
+							</NavItem>
 
-              <NavItem>
-                <NavLinks
-                  onClick={handleClick}
-                  style={{
-                    color: '#ffff',
-                    fontfamily: ' clone-rounded-latin, sans-serif',
-                  }}
-                  to="team"
-                >
-                  TEAM
-                </NavLinks>
-              </NavItem>
+							<NavItem>
+								<NavLinks
+									onClick={handleClick}
+									style={{
+										color: '#ffff',
+										fontfamily:
+											' clone-rounded-latin, sans-serif',
+									}}
+									to="team"
+								>
+									TEAM
+								</NavLinks>
+							</NavItem>
 
-              <NavItem>
-                <NavLinks onClick={() => setButton(!button)}>
-                  <NavItemBtn>
-                    {button ? (
-                      <a
-                        href="https://www.eventbrite.co.uk/e/r-u-hacking-2021-24-hour-student-hackathon-tickets-71648722257"
-                        target="_blank"
-                      >
-                        <Button
-                          style={{
-                            fontFamily: ' clone-rounded-latin, sans-serif',
-                          }}
-                        >
-                          {' '}
-                          SIGN UP
-                        </Button>
-                      </a>
-                    ) : (
-                      <NavBtnLink to="/sing=up">
-                        <Button onClick={ICON} fontBig primary></Button>
-                      </NavBtnLink>
-                    )}
-                  </NavItemBtn>
-                </NavLinks>
-              </NavItem>
-            </NavMenu>
-          </NavbarContain>
+							<NavItem>
+								<NavItemBtn>
+									<a
+										href="https://www.eventbrite.co.uk/e/r-u-hacking-2022-24-hour-student-hackathon-tickets-236290038867"
+										target="_blank"
+										rel="noopener noreferrer"
+									>
+										<Button
+											style={{
+												fontFamily:
+													' clone-rounded-latin, sans-serif',
+											}}
+										>
+											{' '}
+											SIGN UP
+										</Button>
+									</a>
+								</NavItemBtn>
+							</NavItem>
+						</NavMenu>
+					</NavbarContain>
 
-          {/* TODO: Add new MLH badge here */}
-          {/* <NavLogo>
-            <NavLinks>
-              <img
-                src="https://s3.amazonaws.com/logged-assets/trust-badge/2021/mlh-trust-badge-2021-white.svg"
-                alt="Major League Hacking 2021 Hackathon Season"
-                style={{
-                  width: 'auto',
-                  height: 120,
-                  marginRight: 7,
-                  marginTop: 40,
-                  zIndex: 1,
-                }}
-              />
-            </NavLinks>
-          </NavLogo> */}
-        </Nav>
-      </IconContext.Provider>
-    </>
-  );
+					{/* TODO: Add new MLH badge here */}
+					<a
+						href="https://mlh.io/seasons/2022/events?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2022-season&utm_content=white"
+						id="mlh-trust-badge"
+						style={{
+							display: 'block',
+							width: '90px',
+							height: '100%',
+							marginRight: '15px',
+							zIndex: '10000',
+						}}
+						target="_blank"
+						rel="noreferrer noopener"
+					>
+						<img
+							src="https://s3.amazonaws.com/logged-assets/trust-badge/2022/mlh-trust-badge-2022-white.svg"
+							alt="Major League Hacking 2022 Hackathon Season"
+						/>
+					</a>
+				</Nav>
+			</IconContext.Provider>
+		</>
+	);
 };
 
 export default Navbar;
--- src/components/Navbar/navbar.elements.js
+++ src/components/Navbar/navbar.elements.js
@@ -1,139 +1,131 @@
-import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import { Link as LinkS } from 'react-scroll';
+import styled from 'styled-components';
 import { Container } from '../../global_styles';
 
 export const Nav = styled.nav`
-  background: #454545;
-  height: 80px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 1rem;
-  position: sticky;
-  top: 0;
-  z-index: 999;
+	background: #454545;
+	height: 80px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 1rem;
+	position: sticky;
+	top: 0;
+	z-index: 999;
 `;
 
 export const NavbarContain = styled(Container)`
-  display: flex;
-  justify-content: space-between;
-  height: 80px;
+	display: flex;
+	justify-content: space-between;
+	height: 80px;
 
-  ${Container}
+	${Container}
 `;
 
 export const NavLogo = styled(Link)`
-  color: #ffff;
-  justify-self: flex-start;
-  cursor: pointer;
-  text-decoration: none;
-  font-size: 1.5rem;
-  display: flex;
-  align-items: center;
+	color: #ffff;
+	justify-self: flex-start;
+	cursor: pointer;
+	text-decoration: none;
+	font-size: 1.5rem;
+	display: flex;
+	align-items: center;
 `;
 
 export const ICON = styled.div`
-  display: none;
-  
+	display: none;
 
-  @media screen and (max-width: 960px) {
-    display: block;
-    position: absolute;
-    top: 0;
-    right: 0;
-    transform: translate(-400%, 60%);
-    font-size: 1.8rem;
-    cursor: pointer;
-  }
+	@media screen and (max-width: 960px) {
+		display: block;
+		position: absolute;
+		top: 0;
+		right: 0;
+		transform: translate(-400%, 60%);
+		font-size: 1.8rem;
+		cursor: pointer;
+	}
 `;
 
 export const NavMenu = styled.ul`
-  display: flex;
-  align-items: center;
-  list-style: none;
-  text-align: center;
+	display: flex;
+	align-items: center;
+	list-style: none;
+	text-align: center;
 
-  @media screen and (max-width: 960px) {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 120vh;
-    position: absolute;
-    top: 80px;
-    left: ${({ click }) => (click ? 0 : '-100%')};
-    opacity: 1;
-    transition: all 0.5s ease;
-    background: #454545;
-  }
+	@media screen and (max-width: 960px) {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 120vh;
+		position: absolute;
+		top: 80px;
+		left: ${({ click }) => (click ? 0 : '-100%')};
+		opacity: 1;
+		transition: all 0.5s ease;
+		background: #454545;
+	}
 `;
 
 export const NavItem = styled.li`
-  height: 80px;
-  cursor: pointer;
+	cursor: pointer;
 
+	border-bottom: 2px solid transparent;
+	&:hover {
+		border-bottom: 2px solid none;
+	}
 
-  border-bottom: 2px solid transparent;
-  &:hover {
-    border-bottom: 2px solid none;
-  }
+	@media screen and (max-witdh: 960px) {
+		width: 100%;
 
-  @media screen and (max-witdh: 960px) {
-    width: 100%;
-
-    &:hover {
-      border: #000000;
-    }
-  }
+		&:hover {
+			border: #000000;
+		}
+	}
 `;
 export const NavLinks = styled(LinkS)`
-  color: #fff;
-  display: flex;
-  align-items: center;
-  text-decoration: none;
-  padding: 0.5rem 1rem;
-  height: 100%;
+	color: #fff;
+	display: flex;
+	align-items: center;
+	text-decoration: none;
+	padding: 0.5rem 1rem;
+	height: 100%;
 
-  @media screen and (max-witdh: 960px) {
-    text-align: center;
-    padding: 2rem;
-    width: 100%;
-    display: table;
+	@media screen and (max-witdh: 960px) {
+		text-align: center;
+		padding: 2rem;
+		width: 100%;
+		display: table;
 
-    &:hover {
-      color: none;
-      transition: all 0.3s ease;
-    }
-  }
+		&:hover {
+			color: none;
+			transition: all 0.3s ease;
+		}
+	}
 `;
 
 export const NavItemBtn = styled.li`
-  @media screen and (max-witdh: 960px) {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 180px;
-
-  }
+	@media screen and (max-witdh: 960px) {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100%;
+		height: 180px;
+	}
 `;
 export const NavBtnLink = styled(LinkS)`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-decoration: none;
-  padding: 8px 16px;
-  height: 100%;
-  width: 100%;
-  border: none;
-  outline: none;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	text-decoration: none;
+	padding: 8px 16px;
+	height: 100%;
+	width: 100%;
+	border: none;
+	outline: none;
 `;
 
-
-
-
 export const SmallMarging = styled.p`
-  margin-bottom:50px;
-  background:#454545;
-
-`
+	margin-bottom: 50px;
+	background: #454545;
+`;
