1777314369ce10c935faf6e6740d9905da937f30
ej016289 <ej016289@student.reading.ac.uk>
2022-06-16 19:48:19 +0100

    WIP updates sponsors

--- src/components/Sponsors/Sponsors.js
+++ src/components/Sponsors/Sponsors.js
@@ -1,145 +1,151 @@
-import React from 'react';
-import {
-  InfoSec,
-  InfoRow,
-  ImgWrapper,
-  SponsorLinksContainer,
-  SponsorLinksWrapper,
-  SponsorLinksItems,
-  SponsorLinkTitle,
-  SponsorContainer,
-} from './Sponsors.elements';
-import Zebra from '../../img/Zebra_Technologies.png';
+import Image from 'react-bootstrap/Image';
 import S2Oi from '../../img/2Oi.png';
+import Bayer from '../../img/Bayer_LifeHub_UK_Logo.jpg';
 import HackathonsUk from '../../img/HackathonsUk.svg';
 import ThamesValleyAiHub from '../../img/ThamesValleyAiHub.png';
-import Bayer from '../../img/Bayer_LifeHub_UK_Logo.jpg';
-import { TopLine, GapForSponsors } from './Sponsors.elements';
-import Institute_of_Technology from '../../img/Institute_of_Technology_-_Gold_Sponsor.png';
-import BCS from '../../img/bcs-logo.svg';
-import Image from 'react-bootstrap/Image';
+import Zebra from '../../img/Zebra_Technologies.png';
+import { GapForSponsors, ImgWrapper, InfoRow, InfoSec, SponsorContainer, SponsorLinksContainer, SponsorLinksItems, SponsorLinksWrapper, SponsorLinkTitle, TopLine } from './Sponsors.elements';
 
 const Sponsors = ({ lightBg, imgStart, alt, start }) => {
-  return (
-    <>
- <link rel="stylesheet" href="https://use.typekit.net/vtd1gyc.css"></link>
+	return (
+		<>
+			<link
+				rel="stylesheet"
+				href="https://use.typekit.net/vtd1gyc.css"
+			></link>
 
-      <InfoSec lightBg={lightBg}>
-        <SponsorContainer>
-          <SponsorLinksContainer>
-            <SponsorLinksWrapper>
-              <SponsorLinksItems>
-                <TopLine></TopLine>
-                <SponsorLinkTitle
-                  style={{
-                    color: '#000000',
-                  }}
-                >
-                  SPONSORS
-                  <TopLine></TopLine>
-                </SponsorLinkTitle>
-                <SponsorLinkTitle
-                  style={{
-                    color: '#FFD700',
+			<InfoSec lightBg={lightBg}>
+				<SponsorContainer>
+					<SponsorLinksContainer>
+						<SponsorLinksWrapper>
+							<SponsorLinksItems>
+								<TopLine />
+								<SponsorLinkTitle
+									style={{
+										color: '#000000',
+									}}
+								>
+									SPONSORS
+									<TopLine />
+								</SponsorLinkTitle>
+								<SponsorLinkTitle
+									style={{
+										color: '#FFD700',
+									}}
+								>
+									Gold
+									<InfoRow imgStart={imgStart}>
+										<a
+											href="https://innovate.bayer.com/lifehub-uk"
+											target="_blank"
+										>
+											<ImgWrapper start={start}>
+												<Image
+													src={Bayer}
+													fluid
+													alt={alt}
+												/>
+											</ImgWrapper>
+										</a>
 
-                  }}
-                >
-                  
-                  Gold
+										<GapForSponsors />
+										<ImgWrapper start={start}>
+											<a
+												href="https://www.zebra.com/us/en.html/"
+												target="_blank"
+											>
+												<Image
+													src={Zebra}
+													fluid
+													alt={alt}
+												/>
+											</a>
+										</ImgWrapper>
+									</InfoRow>
+								</SponsorLinkTitle>
 
-                  <InfoRow imgStart={imgStart}>
-                  
+								<TopLine />
+								<SponsorLinkTitle
+									style={{
+										color: '#C0C0C0',
+									}}
+								>
+									Silver
+									<ImgWrapper start={start}></ImgWrapper>
+									<InfoRow imgStart={imgStart}></InfoRow>
+								</SponsorLinkTitle>
 
-                    <a href="https://southcentraliot.co.uk/" target="_blank">
-                      <ImgWrapper start={start}>
-                       <Image src={Institute_of_Technology} fluid   alt={alt} />
-                      </ImgWrapper>
-                    </a>
-                    <GapForSponsors></GapForSponsors>
-                    <a href="https://innovate.bayer.com/lifehub-uk" target="_blank">
-                      <ImgWrapper start={start}>
-                        <Image src={Bayer} fluid alt={alt} />
-                      </ImgWrapper>
-                    </a>
+								<TopLine />
 
-                    <GapForSponsors></GapForSponsors>
-                    <ImgWrapper start={start}>
-                      <a href="https://www.zebra.com/us/en.html/" target="_blank">
-                        <Image src={Zebra} fluid alt={alt} />
-                      </a>
-                    </ImgWrapper>
+								<SponsorLinkTitle
+									style={{
+										color: '#CD7F32',
+									}}
+								>
+									Bronze
+									<InfoRow imgStart={imgStart}>
+										<ImgWrapper start={start}>
+											<a
+												href="https://www.20i.com/"
+												target="_blank"
+											>
+												<Image
+													src={S2Oi}
+													fluid
+													alt={alt}
+												/>
+											</a>
+										</ImgWrapper>
+									</InfoRow>
+								</SponsorLinkTitle>
 
-                  </InfoRow>
-                  </SponsorLinkTitle>
+								<TopLine />
 
+								<SponsorLinkTitle
+									style={{
+										color: '#000000',
+									}}
+								>
+									Partners
+									<InfoRow imgStart={imgStart}>
+										<a
+											href="https://hackathons.org.uk/"
+											target="_blank"
+										>
+											<ImgWrapper start={start}>
+												<Image
+													src={HackathonsUk}
+													fluid
+													alt={alt}
+												/>
+											</ImgWrapper>
+										</a>
 
-                <TopLine></TopLine>
-                <SponsorLinkTitle
-                  style={{
+										<GapForSponsors></GapForSponsors>
 
-                    
-                    color: '#C0C0C0',
-                  }}
-                >
-                  Silver
-                  <ImgWrapper start={start}></ImgWrapper>
-                  <InfoRow imgStart={imgStart}></InfoRow>
-                </SponsorLinkTitle>
+										<ImgWrapper start={start}>
+											<a
+												href="https://www.tvaihub.co.uk/"
+												target="_blank"
+											>
+												<Image
+													src={ThamesValleyAiHub}
+													fluid
+													alt={alt}
+												/>
+											</a>
+										</ImgWrapper>
 
-                <TopLine></TopLine>
-
-                <SponsorLinkTitle
-                  style={{
-                    color: '#CD7F32',
-                  }}
-                >
-                  Bronze
-                  <InfoRow imgStart={imgStart}>
-
-
-                    <ImgWrapper start={start}>
-                      <a href="https://www.20i.com/" target="_blank">
-                        <Image src={S2Oi} fluid alt={alt} />
-                      </a>
-                    </ImgWrapper>
-                  </InfoRow>
-                </SponsorLinkTitle>
-
-                <TopLine></TopLine>
-
-                <SponsorLinkTitle
-                  style={{
-                    color: '#000000',
-                  }}
-                >
-                  Partners
-                  <InfoRow imgStart={imgStart}>
-                    <a href="https://hackathons.org.uk/" target="_blank">
-                      <ImgWrapper start={start}>
-                        <Image src={HackathonsUk} fluid alt={alt} />
-                      </ImgWrapper>
-                    </a>
-
-                    <GapForSponsors></GapForSponsors>
-
-                    <ImgWrapper start={start}>
-                      <a href="https://www.tvaihub.co.uk/" target="_blank">
-                        <Image src={ThamesValleyAiHub} fluid alt={alt} />
-                      </a>
-                    </ImgWrapper>
-
-                    <GapForSponsors></GapForSponsors>
-
-
-                  </InfoRow>
-                </SponsorLinkTitle>
-              </SponsorLinksItems>
-            </SponsorLinksWrapper>
-          </SponsorLinksContainer>
-        </SponsorContainer>
-      </InfoSec>
-      </>
-  );
+										<GapForSponsors></GapForSponsors>
+									</InfoRow>
+								</SponsorLinkTitle>
+							</SponsorLinksItems>
+						</SponsorLinksWrapper>
+					</SponsorLinksContainer>
+				</SponsorContainer>
+			</InfoSec>
+		</>
+	);
 };
 
 export default Sponsors;
--- src/components/Sponsors1/Sponsors1.js
+++ src/components/Sponsors1/Sponsors1.js
@@ -1,247 +1,329 @@
-import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import { Container, Card, CardGroup, CardDeck } from 'react-bootstrap';
+import { Card, CardDeck, CardGroup, Container } from 'react-bootstrap';
 import { Button } from '../../global_styles';
 
-import { TopLine,SmallTopLine,Title,InfoRow } from './Sponsors1.elements';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { InfoRow, SmallTopLine, Title, TopLine } from './Sponsors1.elements';
 
+import Image from 'react-bootstrap/Image';
 
-import Image from 'react-bootstrap/Image'
-
-import Zebra from '../../img/Zebra_Technologies.png';
-import S2Oi from '../../img/2Oi.png';
-import HackathonsUk from '../../img/HackathonsUk.png';
-import ThamesValleyAiHub from '../../img/ThamesValleyAiHub (1) (2).png';
 import Bayer from '../../img/Bayer_LifeHub_UK_Logo.png';
+import BCS from '../../img/bcs-logo.svg';
+import HackathonsUk from '../../img/HackathonsUk.png';
 import Institute_of_Technology from '../../img/Institute_of_Technology_-_Gold_Sponsor.png';
 import mlh from '../../img/mlh-logo-color.png';
-import BCS from '../../img/bcs-logo.svg';
-import Citrix from '../../img/Citrix_Logo_Trademark_adobespark.png';
+import ThamesValleyAiHub from '../../img/ThamesValleyAiHub (1) (2).png';
+import Zebra from '../../img/Zebra_Technologies.png';
 
-import POSTMAN from '../../img/postman-logo-stacked.svg';
-
-import StickerMule from '../../img/Sticker_Mule_Logo.png';
-import Cirrus from '../../img/Cirrus_Logo.png';
-import SEROCU from '../../img/SEROCU_logo-1.png';
-import SSH from '../../img/SSH.png';
 import CSA from '../../img/Comp_Sci_affiliation.png';
 import hackproduction from '../../img/hackproduction.png';
+import SEROCU from '../../img/SEROCU_logo-1.png';
+import SSH from '../../img/SSH.png';
+import StickerMule from '../../img/Sticker_Mule_Logo.png';
 
+const Team1 = ({ lightBg, imgStart, alt, start }) => {
+	return (
+		<Container>
+			<Container>
+				<Title>Sponsors </Title>
+			</Container>
 
-const Team1 = ({ lightBg, imgStart, alt, start } ) => {
-  return (
-    <Container >
-    <Container>
+			<Container>
+				<SmallTopLine
+					style={{
+						fontFamily: 'clone-rounded-latin,sans-serif',
+						color: '#FFD700',
+					}}
+				>
+					Gold
+				</SmallTopLine>
+				<CardGroup>
+					<CardDeck>
+						<InfoRow>
+							<Card
+								style={{
+									width: 'auto',
+									height: 'auto',
+									border: 'none',
+								}}
+							>
+								<a
+									href="https://southcentraliot.co.uk/"
+									target="_blank"
+								>
+									<Image
+										src={Institute_of_Technology}
+										fluid
+										style={{
+											width: '100%',
+											height: 'auto',
+										}}
+									/>
+								</a>
+								<Card.Body
+									style={{
+										width: 'auto',
+										height: 'auto',
+										backgroundColor: '#ffff',
+									}}
+								></Card.Body>
+							</Card>
+						</InfoRow>
 
-      <Title >
-        Sponsors  </Title >
-        </Container>
+						<InfoRow>
+							<Card
+								style={{
+									width: 'auto',
+									height: 'auto',
+									border: 'none',
+								}}
+							>
+								<a
+									href="https://innovate.bayer.com/lifehub-uk"
+									target="_blank"
+								>
+									<Image
+										src={Bayer}
+										fluid
+										style={{
+											width: 'auto',
+											height: 'auto',
+										}}
+									/>
+								</a>
+								<Card.Body
+									style={{
+										width: 'auto',
+										height: 'auto',
+										backgroundColor: '#ffff',
+									}}
+								></Card.Body>
+							</Card>
+						</InfoRow>
 
+						<InfoRow>
+							<Card
+								style={{
+									width: 'auto',
+									height: 'auto',
+									border: 'none',
+								}}
+							>
+								<a
+									href="https://www.zebra.com/us/en.html/"
+									target="_blank"
+								>
+									<Image
+										src={Zebra}
+										fluid
+										style={{
+											width: 'auto',
+											height: 'auto',
+										}}
+									/>
+								</a>
+								<Card.Body
+									style={{
+										width: 'auto',
+										height: 'auto',
+										backgroundColor: '#ffff',
+									}}
+								></Card.Body>
+							</Card>
+						</InfoRow>
 
-        <Container >
-        <SmallTopLine style={{ fontFamily: 'clone-rounded-latin,sans-serif', color: '#FFD700',}}>
-        Gold  
-        </SmallTopLine>
-        <CardGroup>
-          <CardDeck>
+						<InfoRow>
+							<Card
+								style={{ textAlign: 'center', border: 'none' }}
+							>
+								<a href="https://www.bcs.org/" target="_blank">
+									<Image
+										src={BCS}
+										fluid
+										style={{
+											width: 'auto',
+											height: 'auto',
+										}}
+									/>
+								</a>
+								<Card.Body
+									style={{ width: 'auto', height: 'auto' }}
+								></Card.Body>
+							</Card>
+						</InfoRow>
+					</CardDeck>
+				</CardGroup>
 
-          <InfoRow >
-            <Card style={{width: 'auto', height: 'auto',border: 'none' }}>
-            <a href="https://www.citrix.com/en-gb/" target="_blank">
-              <Image src={Citrix} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto', backgroundColor:'#ffff'}}>
-              </Card.Body>
-            </Card>
-             </InfoRow>
+				<SmallTopLine
+					style={{
+						fontFamily: 'clone-rounded-latin,sans-serif',
+						color: '#C0C0C0',
+					}}
+				>
+					Silver{' '}
+				</SmallTopLine>
 
-          <InfoRow>
-            <Card style={{ width: 'auto', height: 'auto',border: 'none' }}>
-            <a href="https://southcentraliot.co.uk/" target="_blank">
-              <Image src={Institute_of_Technology} fluid style={{ width: '100%', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto', backgroundColor:'#ffff'}}>
-              </Card.Body>
-            </Card>
-           </InfoRow>
+				<CardGroup style={{ width: 'auto', height: 'auto' }}>
+					<a
+						href="https://cdn.discordapp.com/attachments/704356731974844426/821814499946135592/prospectus2021-final_1.pdf"
+						target="_blank"
+					>
+						<Button>
+							Do you want your logo here? Become a sponsor!
+						</Button>
+					</a>
+				</CardGroup>
 
-           <InfoRow >
-            <Card style={{width: 'auto', height: 'auto',border: 'none' }}>
-            <a href="https://innovate.bayer.com/lifehub-uk" target="_blank">
-              <Image src={Bayer} fluid style={{  width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto', backgroundColor:'#ffff'}}>
-              </Card.Body>
-            </Card>
-             </InfoRow>
+				{/* 	<SmallTopLine
+					style={{
+						fontFamily: 'clone-rounded-latin,sans-serif',
+						color: '#CD7F32',
+					}}
+				>
+					Bronze{' '}
+				</SmallTopLine>
 
-             <InfoRow >
-            <Card style={{width: 'auto', height: 'auto',border: 'none' }}>
-            <a href="https://www.zebra.com/us/en.html/" target="_blank">
-              <Image src={Zebra} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto', backgroundColor:'#ffff'}}>
-              </Card.Body>
-            </Card>
-             </InfoRow>
+				<CardGroup style={{ width: '50%', height: 'auto' }}>
+					<CardDeck>
+						
+					</CardDeck>
+				</CardGroup> */}
 
-             <InfoRow >
+				<SmallTopLine
+					style={{ fontFamily: 'clone-rounded-latin,sans-serif' }}
+				>
+					Partners{' '}
+				</SmallTopLine>
 
-             <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://www.bcs.org/" target="_blank">
-              <Image src={BCS} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-            </InfoRow >
+				<CardGroup style={{ width: '100%', height: 'auto' }}>
+					<CardDeck>
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a
+								href="https://www.stickermule.com/unlock?ref_id=0539570701&utm_source=sponsorship&utm_campaign=mlh-sponsorship-2019&utm_medium=referral"
+								target="_blank"
+							>
+								<Image
+									src={StickerMule}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
 
-          </CardDeck>
-        </CardGroup>
-        
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a
+								href="https://mlh.io/seasons/2021/events?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white"
+								target="_blank"
+							>
+								<Image
+									src={mlh}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
 
-         <SmallTopLine style={{ fontFamily: 'clone-rounded-latin,sans-serif',color: '#C0C0C0',}}>
-        Silver  </SmallTopLine >
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a
+								href="https://hackathons.org.uk/"
+								target="_blank"
+							>
+								<Image
+									src={HackathonsUk}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
 
-        <CardGroup style={{ width: 'auto', height: 'auto' }}>
-        <a href="https://cdn.discordapp.com/attachments/704356731974844426/821814499946135592/prospectus2021-final_1.pdf" target="_blank">
-        <Button>
-        Do you want your logo here? Become a sponsor!
-          </Button>
-          </a>          
-        </CardGroup>
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a href="https://swagshiphack.dev/" target="_blank">
+								<Image
+									src={SSH}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
+					</CardDeck>
 
+					<CardDeck>
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a href="https://serocu.police.uk/" target="_blank">
+								<Image
+									src={SEROCU}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
 
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a
+								href="https://www.reading.ac.uk/computer-science/"
+								target="_blank"
+							>
+								<Image
+									src={CSA}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
 
-        <SmallTopLine style={{ fontFamily: 'clone-rounded-latin,sans-serif',color: '#CD7F32'}}>
-        Bronze  </SmallTopLine >
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a
+								href="https://www.tvaihub.co.uk/"
+								target="_blank"
+							>
+								<Image
+									src={ThamesValleyAiHub}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
 
-        <CardGroup style={{ width: '50%', height: 'auto' }}>
-          <CardDeck>
-
-          <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://www.cirrusresponse.com/" target="_blank">
-              <Image src={Cirrus} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://www.20i.com/" target="_blank">
-              <Image src={POSTMAN} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-
-
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://www.20i.com/" target="_blank">
-              <Image src={S2Oi} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-          </CardDeck>
-        </CardGroup>
-
-
-
-
-        <SmallTopLine style={{ fontFamily: 'clone-rounded-latin,sans-serif'}}>
-        Partners  </SmallTopLine >
-
-        <CardGroup style={{ width: '100%', height: 'auto' }}>
-          <CardDeck>
-           
-          <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://www.stickermule.com/unlock?ref_id=0539570701&utm_source=sponsorship&utm_campaign=mlh-sponsorship-2019&utm_medium=referral" target="_blank">
-              <Image src={StickerMule} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://mlh.io/seasons/2021/events?utm_source=eu-hackathon&utm_medium=TrustBadge&utm_campaign=2021-season&utm_content=white" target="_blank">
-              <Image src={mlh} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://hackathons.org.uk/" target="_blank">
-              <Image src={HackathonsUk} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://swagshiphack.dev/" target="_blank">
-              <Image src={SSH} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-
-            </CardDeck>
-
-            <CardDeck>
-
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://serocu.police.uk/" target="_blank">
-              <Image src={SEROCU} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://www.reading.ac.uk/computer-science/" target="_blank">
-              <Image src={CSA} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://www.tvaihub.co.uk/" target="_blank">
-              <Image src={ThamesValleyAiHub} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-            
-
-            <Card style={{ textAlign: 'center',border: 'none' }}>
-            <a href="https://hack.productions/" target="_blank">
-              <Image src={hackproduction} fluid style={{ width: 'auto', height: 'auto' }} />
-              </a>
-              <Card.Body style={{ width: 'auto',height: 'auto'}}>
-              </Card.Body>
-            </Card>
-            
-
-          </CardDeck>
-        </CardGroup>
-      </Container>
-      <TopLine></TopLine>
-      </Container>
-
-
-  );
+						<Card style={{ textAlign: 'center', border: 'none' }}>
+							<a href="https://hack.productions/" target="_blank">
+								<Image
+									src={hackproduction}
+									fluid
+									style={{ width: 'auto', height: 'auto' }}
+								/>
+							</a>
+							<Card.Body
+								style={{ width: 'auto', height: 'auto' }}
+							></Card.Body>
+						</Card>
+					</CardDeck>
+				</CardGroup>
+			</Container>
+			<TopLine></TopLine>
+		</Container>
+	);
 };
 
 export default Team1;
